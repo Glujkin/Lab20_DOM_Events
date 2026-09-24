8be8cb51f167d15af47ff2e70d87a46894400d5b
Glujkin <Kartoheckajaren@gmail.com>
2026-09-25 02:27:07 +0300

    Step 4: modifying DOM content and styles

--- main.js
+++ main.js
@@ -16,3 +16,18 @@ console.log("Элемент по селектору:", paragraph);
 
 console.log("Текст заголовка:", title.textContent);
 console.log("Текст абзаца:", paragraph.textContent);
+
+console.log("\nModifying DOM Content and Styles ");
+
+
+title.textContent = "Добро пожаловать в мир DOM!";
+title.style.color = "#10b981";
+title.style.textAlign = "center";
+
+
+paragraph.style.backgroundColor = "#fef3c7";
+paragraph.style.padding = "15px";
+paragraph.style.borderRadius = "8px";
+paragraph.style.borderLeft = "4px solid #f59e0b";
+
+console.log("Содержимое и стили изменены!");
